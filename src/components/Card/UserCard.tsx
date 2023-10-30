99af588dee404579d77d660fa3590431a5da024a
Krishna <krishna@findcoder.io>
2023-10-30 12:47:55 +0530

    speaker socail links

--- src/components/Card/UserCard.tsx
+++ src/components/Card/UserCard.tsx
@@ -12,10 +12,10 @@ const ImgCard = ({ src, name, title }: Props) => {
     <>
       <div className="max-w-[300px] p-6">
         <div className="text-center group">
-          <div className="relative">
-            <div className="absolute inset-0 group-hover:bg-[#000]/40 group-hover:opacity-100 opacity-20 ">
-              <div className="absolute bottom-0">
-                <div className="flex border jus">
+          <div className="relative w-full">
+            <div className="w-full absolute inset-0 group-hover:bg-[#000]/40 group-hover:opacity-100 opacity-0">
+              <div className="absolute w-full bottom-0">
+                <div className="flex justify-between p-2">
                   <div>
                     <TwitterIcon />
                   </div>
